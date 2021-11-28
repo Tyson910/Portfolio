6cfea860c42b136d28280b35af32167387de692b
Tyson910 <Tyson910@lve.missouristate.edu>
2021-11-28 15:38:44 -0600

    Create 2 column layout on desktop for Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,7 @@
 import { MailIcon } from '@heroicons/react/outline';
-import Image from 'next/image';
 
+// import Image from 'next/image';
+// import Me from '../../public/img/miniScreenshot.jpg';
 import {
   GitHubIcon,
   LinkedInIcon,
@@ -9,54 +10,56 @@ import {
 export default function Hero() {
   // min h 500px
   return (
-    <section className="flex flex-col items-center px-5 gap-5 border-b pb-10">
-      <div className="pt-10">
-        {/* <Image
-          src="/img/miniScreenshot.jpg"
-          alt="My headshot"
-          width={500}
-          height={800}
-        /> */}
+    <section className="flex flex-col items-center px-5 gap-5 lg:grid lg:grid-cols-2 lg:container lg:py-20 lg:mb-20 pb-10 lg:pb-5">
+      <div className="mb-4 lg:m-0 pt-14 lg:pt-10 justify-self-center	">
         <img
           src="/img/miniScreenshot.jpg"
           alt="My Headshot"
-          className="h-48 w-48 object-cover rounded-full shadow-md"
+          className="h-48 w-48 lg:h-96 lg:w-96 border border-black object-cover rounded-full shadow-md"
         />
       </div>
-      <div className="text-center space-y-5">
-        <h1 className="border-b-2 w-max mx-auto text-5xl font-bold">
+      <div className="flex flex-col text-center gap-y-5 lg:gap-y-10">
+        <h1 className="w-max mx-auto text-slate-700 text-5xl lg:text-7xl font-bold lg:no-underline">
           Tyson Suttle
         </h1>
-        <h2 className="text-2xl">
+        <h2 className="text-slate-500 text-2xl lg:text-4xl lg:font-light lg:-mt-5">
           Software & Web Developer
         </h2>
-      </div>
-      <div className="flex gap-x-8 text-gray-700">
+        <SocialMediaLinks />
         <a
-          href="https://linkedin.com/in/tyson-suttle"
-          className="w-12 h-12 hover:text-[#0A66C2]"
+          href="doc/Tyson_S_Resume.pdf"
+          className="mt-8 lg:m-0 max-w-max self-center border border-black px-12 py-3 shadow-md hover:shadow-lg rounded-md bg-zinc-400"
         >
-          <LinkedInIcon />
-        </a>
-        <a
-          href="https://linkedin.com/in/tyson-suttle"
-          className="pt-four w-14 h-14 hover:text-yellow-600"
-        >
-          <MailIcon />
-        </a>
-        <a
-          href="https://linkedin.com/in/tyson-suttle"
-          className="w-12 h-12 hover:text-green-500"
-        >
-          <GitHubIcon />
+          My Resume
         </a>
       </div>
-      <a
-        href="doc/Tyson_S_Resume.pdf"
-        className="border border-black px-12 py-3 shadow-md hover:shadow-lg rounded-md bg-zinc-400"
-      >
-        My Resume
-      </a>
     </section>
+  );
+}
+
+function SocialMediaLinks() {
+  return (
+    <div className="hidden md:flex justify-center gap-x-8 text-gray-700">
+      <a
+        href="https://linkedin.com/in/tyson-suttle"
+        rel="noopener noreferrer"
+        target="_blank"
+        className="w-12 h-12 hover:text-[#0A66C2]"
+      >
+        <LinkedInIcon />
+      </a>
+      <a
+        href="https://linkedin.com/in/tyson-suttle"
+        className="pt-four w-14 h-14 hover:text-yellow-600"
+      >
+        <MailIcon />
+      </a>
+      <a
+        href="https://linkedin.com/in/tyson-suttle"
+        className="w-12 h-12 hover:text-green-500"
+      >
+        <GitHubIcon />
+      </a>
+    </div>
   );
 }
