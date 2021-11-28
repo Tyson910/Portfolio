3cdd97183155020932ea1a6e999aebe5129c2cb0
Tyson910 <Tyson910@lve.missouristate.edu>
2021-11-28 15:41:10 -0600

    add console log with contact info to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,11 @@ import Link from 'next/link';
 import PropTypes from 'prop-types';
 
 export default function NavBar() {
+  // eslint-disable-next-line no-console
+  console.log(`Thanks for visiting my portfolio site! Please, feel free to contact me using any of these options: 
+  📧 Email: TysonS.417@gmail.com 
+  👨🏿‍💻 Github: https://github.com/Tyson910 
+  👨🏿‍💼 LinkedIn: https://linkedin.com/in/tyson-suttle`);
   return (
     <header className="w-full sticky top-0 shadow-md z-10 opacity-[98%] bg-slate-100">
       <nav className="max-w-screen-xl mx-auto flex justify-evenly">
