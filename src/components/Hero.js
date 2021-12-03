6993ee789c1abb632dea528891cabbe4e407ebb8
Tyson910 <Tyson910@lve.missouristate.edu>
2021-12-03 09:21:49 -0500

    use email Icon in Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 
 import Me from '../../public/img/miniScreenshot.jpg';
 import {
+  EmailIcon,
   GitHubIcon,
   LinkedInIcon,
 } from './Icons';
@@ -53,21 +54,17 @@ function SocialMediaLinks() {
         <LinkedInIcon />
       </a>
       <a
-        href="https://linkedin.com/in/tyson-suttle"
+        href='mailto:TysonS.417@gmail.com, subject="Inquiry from website" '
+        rel="noopener noreferrer"
+        target="_blank"
         className="w-14 h-14 hover:drop-shadow hover:text-slate-700"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fill="currentColor"
-            d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"
-          />
-        </svg>
+        <EmailIcon />
       </a>
       <a
-        href="https://linkedin.com/in/tyson-suttle"
+        href="https://github.com/Tyson910"
+        rel="noopener noreferrer"
+        target="_blank"
         className="w-12 h-12 hover:drop-shadow hover:text-slate-700"
       >
         <GitHubIcon />
