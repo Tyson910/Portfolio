fb3fcc54d079e05cc7dce999754fd74131812435
Tyson910 <Tyson910@lve.missouristate.edu>
2021-12-03 14:25:12 -0500

    add purple shades to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
   ],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'purple-shade-1': '#4D1876',
+        'purple-shade-2': '#421565',
+        'purple-shade-3': '#371154',
+        'purple-shade-4': '#2C0E44',
+        'purple-shade-5': '#210A33',
+        'purple-shade-6': '#160722',
+        'purple-shade-7': '#0B0311',
+      },
+    },
   },
   variants: {
     extend: {},
