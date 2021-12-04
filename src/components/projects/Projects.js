0fa3dde380faa9d89231e3ceea5a8a3c1a4cbd20
Tyson910 <Tyson910@lve.missouristate.edu>
2021-12-04 14:09:03 -0500

    decrease padding & margin in AllProjects component

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -67,7 +67,7 @@ export default function AllProjects() {
   return (
     <section className="space-y-10">
       <p
-        className="pt-36 -mt-36 mb-14 text-4xl text-center font-semibold"
+        className="pt-20 -mt-20 mb-14 text-4xl text-center font-semibold"
         id="projects"
       >
         Projects
