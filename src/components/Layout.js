d27cc31cf7076417a995fe877100ce4eee1510a3
Tyson910 <Tyson910@lve.missouristate.edu>
2021-12-05 17:36:26 -0500

    add lang='en' to Layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,6 +14,7 @@ export default function Layout({
           rel="icon"
           href="/img/icons/avi.svg"
         />
+        <html lang="en" />
       </Head>
       <NavBar />
       <main>{children}</main>
