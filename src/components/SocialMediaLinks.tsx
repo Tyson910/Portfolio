b44b9cb9ba5c26aaa53218d8cef72451040af00b
Tyson910 <Tyson910@lve.missouristate.edu>
2022-09-07 21:09:31 -0400

    use SocialMediaLink inside of SocialMediaLinks

--- src/components/SocialMediaLinks.tsx
+++ src/components/SocialMediaLinks.tsx
@@ -3,28 +3,44 @@ import {
   GitHubIcon,
   LinkedInIcon,
 } from '@assets/icons/TechIcons';
-
+import classNames from 'classnames';
 export function SocialMediaLinks(): JSX.Element {
   const iconClasses: string =
     'h-9 w-9 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300 ';
   return (
-    <div className="hidden md:flex gap-x-8 text-slate-900">
-      <a
-        href="https://linkedin.com/in/tyson-suttle"
-        rel="noopener noreferrer"
-        target="_blank"
-        className="w-12 h-12 hover:drop-shadow	btn-animation text-[#0A66C2]"
-        title="LinkedIn profile"
+    <div className="mt-6 flex flex-row gap-6 text-slate-900">
+      <SocialMediaLink
+        href="hhttps://linkedin.com/in/tyson-suttle"
+        title="My LinkedIn profile"
       >
-        <LinkedInIcon />
-      </a>
-      <a
+        <LinkedInIcon
+          className={classNames(
+            iconClasses,
+            'group-hover:fill-[#0A66C2] dark:group-hover:fill-[#0A66C2]'
+          )}
+        />
+      </SocialMediaLink>
+      <SocialMediaLink
         href='mailto:TysonS.417@gmail.com, subject="Inquiry from website" '
-        rel="noopener noreferrer"
-        target="_blank"
-        className="w-14 h-14 hover:drop-shadow hover:text-slate-700 btn-animation"
-        title="My email"
+        title="My Github profile"
       >
+        <EmailIcon
+          className={iconClasses}
+        />
+      </SocialMediaLink>
+
+      <SocialMediaLink
+        href="https://github.com/Tyson910"
+        title="My Github profile"
+      >
+        <GitHubIcon
+          className={iconClasses}
+        />
+      </SocialMediaLink>
+    </div>
+  );
+}
+
 function SocialMediaLink({
   href,
   title,
