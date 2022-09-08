be8a6dc800319fbeb4bd020d116c5cad04e3110c
Tyson910 <Tyson910@lve.missouristate.edu>
2022-09-07 21:02:56 -0400

    prettier fixes

--- src/assets/icons/TechIcons.tsx
+++ src/assets/icons/TechIcons.tsx
@@ -1,6 +1,9 @@
-import { SVGProps } from "react";
+import { SVGProps } from 'react';
 
-export function HTMLIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>): JSX.Element {
+export function HTMLIcon(
+  props: JSX.IntrinsicAttributes &
+    SVGProps<SVGSVGElement>
+): JSX.Element {
   return (
     <svg
       role="img"
@@ -17,7 +20,10 @@ export function HTMLIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement
   );
 }
 
-export function CSSIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>): JSX.Element {
+export function CSSIcon(
+  props: JSX.IntrinsicAttributes &
+    SVGProps<SVGSVGElement>
+): JSX.Element {
   return (
     <svg
       role="img"
@@ -35,7 +41,8 @@ export function CSSIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>
 }
 
 export function JavaScriptIcon(
-  props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>
+  props: JSX.IntrinsicAttributes &
+    SVGProps<SVGSVGElement>
 ): JSX.Element {
   return (
     <svg
@@ -53,7 +60,10 @@ export function JavaScriptIcon(
   );
 }
 
-export function ReactIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>): JSX.Element {
+export function ReactIcon(
+  props: JSX.IntrinsicAttributes &
+    SVGProps<SVGSVGElement>
+): JSX.Element {
   return (
     <svg
       role="img"
@@ -70,7 +80,10 @@ export function ReactIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElemen
   );
 }
 
-export function NextJSIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>): JSX.Element {
+export function NextJSIcon(
+  props: JSX.IntrinsicAttributes &
+    SVGProps<SVGSVGElement>
+): JSX.Element {
   return (
     <svg
       role="img"
@@ -88,7 +101,8 @@ export function NextJSIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGEleme
 }
 
 export function TailwindIcon(
-  props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>
+  props: JSX.IntrinsicAttributes &
+    SVGProps<SVGSVGElement>
 ): JSX.Element {
   return (
     <svg
@@ -106,7 +120,10 @@ export function TailwindIcon(
   );
 }
 
-export function NPMIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>): JSX.Element {
+export function NPMIcon(
+  props: JSX.IntrinsicAttributes &
+    SVGProps<SVGSVGElement>
+): JSX.Element {
   return (
     <svg
       role="img"
@@ -124,7 +141,8 @@ export function NPMIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>
 }
 
 export function TestingLibraryIcon(
-  props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>
+  props: JSX.IntrinsicAttributes &
+    SVGProps<SVGSVGElement>
 ): JSX.Element {
   return (
     <svg
@@ -142,7 +160,10 @@ export function TestingLibraryIcon(
   );
 }
 
-export function JestIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>): JSX.Element {
+export function JestIcon(
+  props: JSX.IntrinsicAttributes &
+    SVGProps<SVGSVGElement>
+): JSX.Element {
   return (
     <svg
       role="img"
@@ -159,7 +180,10 @@ export function JestIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement
   );
 }
 
-export function GitHubIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>): JSX.Element {
+export function GitHubIcon(
+  props: JSX.IntrinsicAttributes &
+    SVGProps<SVGSVGElement>
+): JSX.Element {
   return (
     <svg
       aria-hidden="true"
@@ -170,15 +194,14 @@ export function GitHubIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGEleme
       className={props?.className}
     >
       <title>GitHub</title>
-      <path
-        d="M12 2C6.475 2 2 6.588 2 12.253c0 4.537 2.862 8.369 6.838 9.727.5.09.687-.218.687-.487 0-.243-.013-1.05-.013-1.91C7 20.059 6.35 18.957 6.15 18.38c-.113-.295-.6-1.205-1.025-1.448-.35-.192-.85-.667-.013-.68.788-.012 1.35.744 1.538 1.051.9 1.551 2.338 1.116 2.912.846.088-.666.35-1.115.638-1.371-2.225-.256-4.55-1.14-4.55-5.062 0-1.115.387-2.038 1.025-2.756-.1-.256-.45-1.307.1-2.717 0 0 .837-.269 2.75 1.051.8-.23 1.65-.346 2.5-.346.85 0 1.7.115 2.5.346 1.912-1.333 2.75-1.05 2.75-1.05.55 1.409.2 2.46.1 2.716.637.718 1.025 1.628 1.025 2.756 0 3.934-2.337 4.806-4.562 5.062.362.32.675.936.675 1.897 0 1.371-.013 2.473-.013 2.82 0 .268.188.589.688.486a10.039 10.039 0 0 0 4.932-3.74A10.447 10.447 0 0 0 22 12.253C22 6.588 17.525 2 12 2Z"
-      />
+      <path d="M12 2C6.475 2 2 6.588 2 12.253c0 4.537 2.862 8.369 6.838 9.727.5.09.687-.218.687-.487 0-.243-.013-1.05-.013-1.91C7 20.059 6.35 18.957 6.15 18.38c-.113-.295-.6-1.205-1.025-1.448-.35-.192-.85-.667-.013-.68.788-.012 1.35.744 1.538 1.051.9 1.551 2.338 1.116 2.912.846.088-.666.35-1.115.638-1.371-2.225-.256-4.55-1.14-4.55-5.062 0-1.115.387-2.038 1.025-2.756-.1-.256-.45-1.307.1-2.717 0 0 .837-.269 2.75 1.051.8-.23 1.65-.346 2.5-.346.85 0 1.7.115 2.5.346 1.912-1.333 2.75-1.05 2.75-1.05.55 1.409.2 2.46.1 2.716.637.718 1.025 1.628 1.025 2.756 0 3.934-2.337 4.806-4.562 5.062.362.32.675.936.675 1.897 0 1.371-.013 2.473-.013 2.82 0 .268.188.589.688.486a10.039 10.039 0 0 0 4.932-3.74A10.447 10.447 0 0 0 22 12.253C22 6.588 17.525 2 12 2Z" />
     </svg>
   );
 }
 
 export function GitHubOutlineIcon(
-  props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>
+  props: JSX.IntrinsicAttributes &
+    SVGProps<SVGSVGElement>
 ): JSX.Element {
   return (
     <svg
@@ -220,7 +243,10 @@ export function LinkedInIcon(
   );
 }
 
-export function EmailIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>): JSX.Element {
+export function EmailIcon(
+  props: JSX.IntrinsicAttributes &
+    SVGProps<SVGSVGElement>
+): JSX.Element {
   return (
     <svg
       aria-hidden="true"
