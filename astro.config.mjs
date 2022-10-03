76a375769b6b772677d276e05841a2aaad237477
Tyson <TysonS.417@gmail.com>
2022-10-02 21:46:05 -0400

    add mdx plug-ins to astroconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import image from '@astrojs/image';
 import prefetch from '@astrojs/prefetch';
+import mdx from '@astrojs/mdx';
+import markdownEmoji from 'remark-emoji';
+import { calcReadingTime } from './src/utils/helpers/calc-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +13,13 @@ export default defineConfig({
     image(),
     prefetch({
       // Only prefetch links with an href that begins with `/` (internal links)
-      selector: "a[href^='/']"
+      selector: "a[href^='/']",
+    }),
+    mdx({
+      remarkPlugins: [
+        markdownEmoji,
+        calcReadingTime,
+      ],
     }),
   ],
 });
