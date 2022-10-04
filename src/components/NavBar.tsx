e8fe9750ed6bd30f3b1b6e1f2f2283f0db94d394
Tyson <TysonS.417@gmail.com>
2022-10-04 17:25:29 -0400

    change NavBar to dark background

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -28,20 +28,18 @@ export function NavBar(): JSX.Element {
   return (
     <header
       className={`py-2 w-full sticky top-0 z-10
-   bg-white shadow-md lg:shadow-sm text-purple-shade-4`}
+   shadow-md lg:shadow-sm text-purple-100 bg-purple-shade-4`}
     >
       <nav className="max-w-screen-xl mx-auto flex justify-evenly">
-        {navigationLinks.map(
-          ({ href, text }) => (
-            <a
-              key={href}
-              href={href}
-              className="hover:underline p-4 font-medium"
-            >
-              {text}
-            </a>
-          )
-        )}
+        {navigationLinks.map(({ href, text }) => (
+          <a
+            key={href}
+            href={href}
+            className="hover:underline p-4 font-medium hover:text-white"
+          >
+            {text}
+          </a>
+        ))}
       </nav>
     </header>
   );
