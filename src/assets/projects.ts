7e6688e09f76b035df46d27ad25bb35c9aa7dd86
Tyson <TysonS.417@gmail.com>
2022-12-29 17:51:38 -0600

    add Ragains Floot]ring to projects

--- src/assets/projects.ts
+++ src/assets/projects.ts
@@ -22,6 +22,20 @@ export const projects: Project[] = [
     sourceCodeURL: "https://github.com/Tyson910/InsureLandingPage",
   },
   {
+    projectName: "NBA Stats Tool 1.0",
+    imageURL: "@assets/images/v1-nba-react-app.png",
+    description:
+      "Created a react app to compare up to 5 NBA players stats visually by using a graph either throughout the entire season or between a given date range",
+    techUsed: [
+      "React",
+      "Victory Charts",
+      "Vanilla CSS",
+      "Balldontlie API",
+    ],
+    deployURL: "https://v1-nba-stats-tool.netlify.app/",
+    sourceCodeURL: "https://github.com/Tyson910/nba-stats-tool/tree/v0",
+  },
+  {
     projectName: "NBA Stats Tool 2.0",
     description:
       "Converted my React single page app to a Svelte app powered by Typescript and Vite",
@@ -36,22 +50,11 @@ export const projects: Project[] = [
     sourceCodeURL: "https://github.com/Tyson910/nba-stats-tool",
   },
   {
-    projectName: "NBA Stats Tool (Version 1)",
+    projectName: "Ragain’s Flooring",
     description:
-      "Created a react app to compare up to 5 NBA players stats visually by using a graph either throughout the entire season or between a given date range",
-    techUsed: [
-      "React",
-      "Victory Charts",
-      "Vanilla CSS",
-      "Balldontlie API",
-    ],
-    deployURL: "https://v1-nba-stats-tool.netlify.app/",
-    sourceCodeURL: "https://github.com/Tyson910/nba-stats-tool/tree/v0",
+      "Created a brochure website for a flooring business based in Jefferson City, Missouri. Enabling this small business owner to display their products & services and allow for interested clients to submit inquiries",
+    techUsed: ["Vanilla HTML", "Vanilla CSS", "Vanilla JavaScript"],
+    deployURL: "https://www.RagainsFlooring.com",
+    sourceCodeURL: null,
   },
-  // {
-  //   projectName: "Ragain’s Flooring",
-  //   description: "Created a brochure website for a flooring business based in Jefferson City, Missouri. Enabling this small business owner to display their products & services and allow for interested clients to submit inquiries",
-  //   techUsed: ["Vanilla HTML", "Vanilla CSS", "Vanilla JavaScript"],
-  //   deployURL: "https://www.RagainsFlooring.com"
-  // }
 ];
