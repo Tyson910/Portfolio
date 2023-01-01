3f6bf6cc1ed7271e455ae3a22421ea6a9bc8c670
Tyson <TysonS.417@gmail.com>
2023-01-01 17:23:02 -0500

    update imageURL to public folder

--- src/assets/projects.ts
+++ src/assets/projects.ts
@@ -15,16 +15,28 @@ export interface Project {
 export const projects: Project[] = [
   {
     projectName: "Sample Website",
-    imageURL: "@assets/images/insure-agency.png",
+    imageURL: "/img/projects/insure-agency.png",
     description:
-      "Created a custom template for brochure websites for various types of businesses to display their products & services and allow for interested clients to submit inquiries",
+      "Created a sample template that is availabe to be used by various types of businesses to display their products & services, a contact form to get in touch with a business",
     techUsed: ["Astro", "TailwindCSS", "React"],
     deployURL: "https://insure-agency.netlify.app/",
     sourceCodeURL: "https://github.com/Tyson910/InsureLandingPage",
   },
   {
+    projectName: "Ragain’s Flooring",
+    /** use live url from netlify here */
+    imageURL:
+      "https://screenshot-proxy.netlify.app/f_jpg,w_336/https://d33wubrfki0l68.cloudfront.net/606f2ca1011d0a0007eaeef5/screenshot_2021-04-08-16-17-43-0000.png",
+    description:
+      `A website for a local flooring company in Jeff City, MO. Features a gallery of completed flooring projects,
+        a contact form for requesting estimates, and info about the company's services.`,
+    techUsed: ["Vanilla HTML", "Vanilla CSS", "Vanilla JavaScript"],
+    deployURL: "https://www.RagainsFlooring.com",
+    sourceCodeURL: null,
+  },
+  {
     projectName: "NBA Stats Tool 1.0",
-    imageURL: "@assets/images/v1-nba-react-app.png",
+    imageURL: "/img/projects/v1-nba-react-app.png",
     description:
       "Created a react app to compare up to 5 NBA players stats visually by using a graph either throughout the entire season or between a given date range",
     techUsed: [
@@ -38,7 +50,7 @@ export const projects: Project[] = [
   },
   {
     projectName: "NBA Stats Tool 2.0",
-    imageURL: "@assets/images/v2-nba-react-app.png",
+    imageURL: "/img/projects/v2-nba-react-app.png",
     description:
       "Converted my React app to a Svelte app powered by Typescript and Vite",
     techUsed: [
@@ -50,13 +62,5 @@ export const projects: Project[] = [
     ],
     deployURL: "https://nba-stats-tool.netlify.app/",
     sourceCodeURL: "https://github.com/Tyson910/nba-stats-tool",
-  },
-  {
-    projectName: "Ragain’s Flooring",
-    description:
-      "Created a brochure website for a flooring business based in Jefferson City, Missouri. Enabling this small business owner to display their products & services and allow for interested clients to submit inquiries",
-    techUsed: ["Vanilla HTML", "Vanilla CSS", "Vanilla JavaScript"],
-    deployURL: "https://www.RagainsFlooring.com",
-    sourceCodeURL: null,
   },
 ];
