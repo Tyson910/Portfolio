17ff34ed34ccbee79c6520707832d9e8de3d4a9c
Tyson <TysonS.417@gmail.com>
2023-03-28 18:39:59 -0400

    add new shake reviews

--- src/assets/cookout-milkshakes.ts
+++ src/assets/cookout-milkshakes.ts
@@ -40,7 +40,7 @@ const foodEmojis = {
   "doughnut": "\u{1F369}",
   "cookie": "\u{1F36A}",
   "peanuts": "\u{1F95C}",
-  "blueberries": "\u{1FAD0}"
+  "blueberries": "\u{1FAD0}",
 };
 
 export const ratingObj: Rating = {
@@ -68,7 +68,7 @@ export interface MilkshakeReview {
 export const milkshakeReviews: MilkshakeReview[] = [
   {
     stars: 0,
-    dishName: `Banana ${foodEmojis['banana']} `,
+    dishName: `Banana ${foodEmojis["banana"]} `,
     explanation:
       `Banana is a classic shake flavor but there's nothing about this shake that sticks out in my mind. Maybe it's just me, but I feel
   like banana shines best as a sidekick or complimentary flavor.`,
@@ -76,14 +76,16 @@ export const milkshakeReviews: MilkshakeReview[] = [
   },
   {
     stars: 2,
-    dishName: `Classic Chocolate ${foodEmojis['chocolate bar']}`,
+    dishName: `Classic Chocolate ${foodEmojis["chocolate bar"]}`,
     explanation:
       "Timeless. Immortal. It’s a classic shake flavor for a reason.",
     dateOfReview: new Date(2023, 2, 13),
   },
   {
     stars: 2,
-    dishName: `Banana Berry ${foodEmojis['banana']}${foodEmojis['blueberries']} `,
+    dishName: `Banana Berry ${foodEmojis["banana"]}${
+      foodEmojis["blueberries"]
+    } `,
     explanation:
       `The shake has a nice balance of flavors between the sweet bananas and the tart berries,
      which create a unique and enjoyable taste. Overall, the Banana Berry shake is a pretty good option`,
@@ -91,10 +93,37 @@ export const milkshakeReviews: MilkshakeReview[] = [
   },
   {
     stars: 3,
-    dishName: `Banana Fudge ${foodEmojis['banana']}${foodEmojis['chocolate bar']} `,
+    dishName: `Banana Fudge ${foodEmojis["banana"]}${
+      foodEmojis["chocolate bar"]
+    } `,
     explanation:
       `The shake is like a banana split, but better. The fudge is a perfect compliment to the banana flavor in this shake.
      Arguably, the best shake at cookout`,
     dateOfReview: new Date(2023, 2, 19),
+  },
+  {
+    dateOfReview: new Date(2023, 2, 21),
+    stars: 0,
+    dishName: `Banana Nut ${foodEmojis["banana"]} ${foodEmojis["peanuts"]}`,
+    explanation:
+      "This shake really reminded me of a good banana bread. Tastes good but would only recommend if you're in the mood for banana bread",
+  },
+  {
+    dateOfReview: new Date(2023, 2, 24),
+    stars: -1,
+    dishName: `Double Chocolate ${foodEmojis["chocolate bar"]} ${
+      foodEmojis["chocolate bar"]
+    }`,
+    explanation:
+      "I know what it sounds like but too much chocolate. This is just the regular chocolate shake with twice the amount of syrup as Classic Chocolate. Probably would’ve been better with chocolate chips or a CHOCOLATE ICE CREAM BASE instead of an double the amount of syrup.",
+  },
+  {
+    dateOfReview: new Date(2023, 2, 26),
+    stars: -2,
+    dishName: `Chocolate Cherry${foodEmojis["chocolate bar"]} ${
+      foodEmojis["cherries"]
+    }`,
+    explanation:
+      "The chocolate totally overpowers the cherry and the cherries were too tart (granted I had this shake in late March but my point still stands), which is a shame because this shake had so much potential. Wouldn't recommend",
   },
 ];
