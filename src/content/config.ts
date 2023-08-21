050bf966dd5977bc9001734a889179be479e9f6a
Tyson <TysonS.417@gmail.com>
2023-08-20 21:13:58 -0400

    delete tech blog posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,6 +3,7 @@ import { defineCollection, z } from "astro:content";
 
 // 2. Define your collection(s)
 const foodBlogCollection = defineCollection({
+  type: 'content',
   schema: z.object({
     layout: z.literal("@layouts/FoodBlogLayout.astro"),
     author: z.literal("Tyson Suttle"),
@@ -17,24 +18,25 @@ const foodBlogCollection = defineCollection({
   }),
 });
 
-const techBlogCollection = defineCollection({
-  schema: z.object({
-    layout: z.literal("@layouts/BlogLayout.astro"),
-    meta_description: z.string(),
-    title: z.string(),
-    author: z.literal("Tyson Suttle"),
-    dateCreated: z.coerce.date(),
-    lastUpdated: z.coerce.date(),
-    prevPage: z.nullable(z.string()).optional(),
-    nextPage: z.nullable(z.string()).optional(),
-    tags: z.array(z.string()),
-    draft: z.boolean(),
-  }),
-});
+// const techBlogCollection = defineCollection({
+//   type: 'content',
+//   schema: z.object({
+//     layout: z.literal("@layouts/BlogLayout.astro"),
+//     meta_description: z.string(),
+//     title: z.string(),
+//     author: z.literal("Tyson Suttle"),
+//     dateCreated: z.coerce.date(),
+//     lastUpdated: z.coerce.date(),
+//     prevPage: z.nullable(z.string()).optional(),
+//     nextPage: z.nullable(z.string()).optional(),
+//     tags: z.array(z.string()),
+//     draft: z.boolean(),
+//   }),
+// });
 
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   "food": foodBlogCollection,
-  "tech": techBlogCollection,
+  // "tech": techBlogCollection,
 };
