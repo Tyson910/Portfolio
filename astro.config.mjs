c5345bb19358429f6a0fea024b917851f2674bd5
Tyson <TysonS.417@gmail.com>
2024-06-26 18:40:03 -0700

    upgrade to astro v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,6 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true,
-  },
   integrations: [
     react(),
     prefetch({
