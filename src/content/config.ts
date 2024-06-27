4efb6ec2802231aaea9d3f59c0cc8e502538c6b1
Tyson <TysonS.417@gmail.com>
2024-06-26 21:55:37 -0700

    set up first snippet

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,50 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection, z } from "astro:content";
+import { z, defineCollection } from 'astro:content';
 
 // 2. Define your collection(s)
-// const techBlogCollection = defineCollection({
-//   type: 'content',
+const techBlogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    layout: z
+      .literal('@layouts/BlogLayout.astro')
+      .default('@layouts/BlogLayout.astro'),
+    meta_description: z.string(),
+    title: z.string(),
+    author: z.literal('Tyson Suttle').default('Tyson Suttle'),
+    dateCreated: z.string().date(),
+    lastUpdated: z.string().date(),
+    prevPage: z.string().nullable(),
+    nextPage: z.string().nullable(),
+    tags: z.array(z.string()),
+    draft: z.boolean(),
+  }),
+});
+
+const techSnippetsCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    meta_description: z.string(),
+    title: z.string(),
+    author: z.literal('Tyson Suttle').default('Tyson Suttle'),
+    dateCreated: z.string().date(),
+    lastUpdated: z.string().date(),
+    tags: z.string().array(),
+    draft: z.boolean(),
+  }),
+});
+
+// TODO: circle back to this
+// const projectsCollection = defineCollection({
+//   type: 'data',
 //   schema: z.object({
-//     layout: z.literal("@layouts/BlogLayout.astro"),
+//     layout: z.literal('@layouts/BlogLayout.astro'),
 //     meta_description: z.string(),
 //     title: z.string(),
-//     author: z.literal("Tyson Suttle"),
+//     author: z.literal('Tyson Suttle').default('Tyson Suttle'),
 //     dateCreated: z.coerce.date(),
 //     lastUpdated: z.coerce.date(),
-//     prevPage: z.nullable(z.string()).optional(),
-//     nextPage: z.nullable(z.string()).optional(),
+//     prevPage: z.string().nullable(),
+//     nextPage: z.string().nullable(),
 //     tags: z.array(z.string()),
 //     draft: z.boolean(),
 //   }),
@@ -22,4 +54,5 @@ import { defineCollection, z } from "astro:content";
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   // "tech": techBlogCollection,
+  snippets: techSnippetsCollection,
 };
