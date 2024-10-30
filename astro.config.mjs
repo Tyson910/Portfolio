01126f89dc6e8e3235b9cb3bc2bf2a35312cb1a6
Tyson <TysonS.417@gmail.com>
2024-10-29 22:10:39 -0700

    remove unused prefetch lib

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import prefetch from '@astrojs/prefetch';
 import mdx from '@astrojs/mdx';
 import markdownEmoji from 'remark-emoji';
 import { calcReadingTime } from './src/utils/frontmatter-helpers/calc-reading-time.mjs';
@@ -9,12 +8,9 @@ import expressiveCode from 'astro-expressive-code';
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   integrations: [
     react(),
-    prefetch({
-      // Only prefetch links with an href that begins with `/` (internal links)
-      selector: "a[href^='/']",
-    }),
     expressiveCode(),
     svelte(),
     mdx({
