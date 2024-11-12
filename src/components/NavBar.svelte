5d3772b31727289253468d39fc8c37c831c4857e
Tyson <TysonS.417@gmail.com>
2024-11-11 20:09:44 -0700

    remove overlay logic from NavBar

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -27,69 +27,19 @@
       text: 'Contact',
     },
   ] as const satisfies NavigationLink[];
-
-  let showOverlay = $state(false);
 </script>
 
-<header class="hidden md:block py-2 w-full sticky top-0 z-10">
+<header class="py-2 w-full sticky top-0 z-10">
   <nav
     class="container max-w-max gap-x-6 px-8 flex justify-evenly text-purple-100 bg-purple-shade-4 shadow-md lg:shadow-sm rounded-full"
   >
     {#each navigationLinks as { href, text } (href)}
-      <a {href} class="hover:underline py-4 font-medium hover:text-white">
+      <a
+        {href}
+        class="hover:underline py-4 text-sm font-medium hover:text-white"
+      >
         {text}
       </a>
     {/each}
   </nav>
 </header>
-
-<!-- Mobile NavBar (hidden on md: screen breakpoint) -->
-{#if showOverlay}
-  <!-- svelte-ignore a11y_click_events_have_key_events -->
-  <!-- svelte-ignore a11y_no_static_element_interactions -->
-  <div
-    class="fixed opacity-90 inset-0 bg-black md:hidden"
-    onclick={() => (showOverlay = false)}
-  >
-    <nav class="text-center opacity-90 mt-40 flex flex-col">
-      {#each navigationLinks as { href, text } (href)}
-        <a
-          {href}
-          class="w-max mx-auto text-4xl p-4 font-medium text-white"
-        >
-          {text}
-        </a>
-      {/each}
-    </nav>
-  </div>
-{:else}
-  <header
-    class={`md:hidden py-2 px-8 w-full sticky top-0 z-10
-   shadow-md text-purple-100 bg-purple-shade-4`}
-  >
-    <nav class="flex items-center justify-between">
-      {#each navigationLinks.slice(0, 1) as { href, text } (href)}
-        <a {href} class="hover:underline p-4 font-medium hover:text-white">
-          {text}
-        </a>
-      {/each}
-      <button onclick={() => (showOverlay = true)}>
-        <!-- Heroicons/outline bars-3 -->
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
-      </button>
-    </nav>
-  </header>
-{/if}
