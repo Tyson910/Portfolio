91d93f8adb72bd46b1eee085429b4786329129db
Tyson <TysonS.417@gmail.com>
2024-11-11 19:40:30 -0700

    update footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,36 +1,24 @@
-export function Footer(): JSX.Element {
+import { socialLinks } from '@utils/helpers';
+
+export function Footer() {
   return (
-    <footer className="w-full text-purple-100 bg-purple-shade-4 py-14">
-      <div className="container space-y-10">
-        <p
-          className="text-3xl md:text-4xl font-medium "
-          id="contact"
-        >
-          Contact
-        </p>
-        <p className="text-lg max-w-screen-sm">
-          Whether you have a question or
-          just want to chat, you can{' '}
-          <a
-            href="https://linkedin.com/in/tyson-suttle"
-            rel="noopener noreferrer"
-            target="_blank"
-            title="LinkedIn profile"
-            className="underline hover:text-slate-300"
-          >
-            send me a message
-          </a>{' '}
-          on LinkedIn or shoot me an
-          email:
-          {'  '}
-          <a
-            href="mailto:TysonS.417@gmail.com?subject=Inquiry from website"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="underline hover:text-slate-300"
-          >
-            TysonS.417@gmail.com
-          </a>
+    <footer className="border-t border-zinc-100 dark:border-zinc-700/40">
+      <div className="container py-12 md:flex md:items-center md:justify-between">
+        <div className="flex justify-center gap-x-6 md:order-2">
+          {socialLinks.map(({ Icon, ariaLabel, href }) => (
+            <a
+              key={href}
+              href={href}
+              className="text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-300"
+            >
+              <span className="sr-only">{ariaLabel}</span>
+              <Icon aria-hidden="true" className="size-6" />
+            </a>
+          ))}
+        </div>
+        <p className="mt-8 text-center text-sm/6 text-gray-600 dark:text-gray-400 md:order-1 md:mt-0">
+          &copy; {new Date().getFullYear()} Tyson Suttle. All rights
+          reserved.
         </p>
       </div>
     </footer>
