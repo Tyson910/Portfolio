0061c392d297c05fd06a6199bc4fc7d6618b7c5e
Tyson <TysonS.417@gmail.com>
2024-12-02 23:22:20 -0700

    add string util functions to helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -96,3 +96,24 @@ export const projects = [
   //   sourceCodeURL: "https://github.com/Tyson910/InsureLandingPage",
   // },
 ] as const satisfies Project[];
+
+function capitalizeFirstLetter(str: string) {
+  return str[0].toUpperCase() + str.substring(1);
+}
+
+export function deSlugifyStr(str: string) {
+  // if string doesn't contain a '-' or '_' just return back the capitalized word
+  if (!str.includes('-') && !str.includes('_'))
+    return capitalizeFirstLetter(str);
+
+  // replaces '-' or '_ with ' '
+  const StrWithDashesAndHypensReplacedWithSpaces = str
+    .replace(/-/g, '  ')
+    .replace(/_/g, '  ');
+
+  let deSlugifiedStr = '';
+  StrWithDashesAndHypensReplacedWithSpaces.split(' ').forEach((char) => {
+    deSlugifiedStr += !!char ? capitalizeFirstLetter(char) : ' ';
+  });
+  return deSlugifiedStr;
+}
