f4470e08cdad6974a8fd4e72e0ad1ddf155472d6
Tyson <TysonS.417@gmail.com>
2024-12-09 23:23:52 -0700

    use src property for images

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,23 +3,23 @@ import { z, defineCollection } from 'astro:content';
 import { file } from 'astro/loaders';
 
 // 2. Define your collection(s)
-const techBlogCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    layout: z
-      .literal('@layouts/BlogLayout.astro')
-      .default('@layouts/BlogLayout.astro'),
-    meta_description: z.string(),
-    title: z.string(),
-    author: z.literal('Tyson Suttle').default('Tyson Suttle'),
-    dateCreated: z.string().date(),
-    lastUpdated: z.string().date(),
-    prevPage: z.string().nullable(),
-    nextPage: z.string().nullable(),
-    tags: z.array(z.string()),
-    draft: z.boolean(),
-  }),
-});
+// const techBlogCollection = defineCollection({
+//   type: 'content',
+//   schema: z.object({
+//     layout: z
+//       .literal('@layouts/BlogLayout.astro')
+//       .default('@layouts/BlogLayout.astro'),
+//     meta_description: z.string(),
+//     title: z.string(),
+//     author: z.literal('Tyson Suttle').default('Tyson Suttle'),
+//     dateCreated: z.string().date(),
+//     lastUpdated: z.string().date(),
+//     prevPage: z.string().nullable(),
+//     nextPage: z.string().nullable(),
+//     tags: z.array(z.string()),
+//     draft: z.boolean(),
+//   }),
+// });
 
 const techSnippetsCollection = defineCollection({
   type: 'content',
@@ -37,31 +37,30 @@ const techSnippetsCollection = defineCollection({
 });
 
 const filmCollection = defineCollection({
-  // type: 'data',
   loader: file('src/content/photos.json'),
-  schema: z.object({
-    date: z.string().datetime(),
-    location: z.string(),
-    description: z.string().optional(),
-    tags: z
-      .enum([
-        'Landscape',
-        'Architecture',
-        'Art',
-        'Graffiti',
-        'Nature / Wildlife',
-        'Misc',
-      ])
-      .array(),
-    filmType: z
-      .enum([
+  schema: ({ image }) =>
+    z.object({
+      date: z.string().datetime(),
+      src: image(),
+      location: z.string(),
+      description: z.string().optional(),
+      tags: z
+        .enum([
+          'Landscape',
+          'Architecture',
+          'Art',
+          'Graffiti',
+          'Nature / Wildlife',
+          'Misc',
+        ])
+        .array(),
+      filmType: z.enum([
         'Kodak Gold 200',
         'Kodak UltraMax 400',
         'Kodak Gold 800 (Disposable)',
         'Fuji Film 400',
       ]),
-    // draft: z.boolean(),
-  }),
+    }),
 });
 
 // TODO: circle back to this
