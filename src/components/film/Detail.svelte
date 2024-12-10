f95b99738eb8637235070127616d84e471151890
Tyson <TysonS.417@gmail.com>
2024-12-09 17:36:58 -0700

    create Detail helper component

---
+++ src/components/film/Detail.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+  import type CalendarIcon from '~icons/ri/calendar-line';
+
+  let {
+    Icon,
+    children,
+  }: {
+    Icon: typeof CalendarIcon;
+    children: Snippet;
+  } = $props();
+</script>
+
+<div class="flex items-center text-sm text-gray-600">
+  <Icon class="mr-2 size-4 text-gray-500" />
+  <span>
+    {@render children()}
+  </span>
+</div>
