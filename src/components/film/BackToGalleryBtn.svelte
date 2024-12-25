6fa7645c8b9f22c9279593a6d40f2cc86d40405a
Tyson <TysonS.417@gmail.com>
2024-12-25 10:28:07 -0700

    create BackToGalleryBtn button

---
+++ src/components/film/BackToGalleryBtn.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import ArrowLeftIcon from '~icons/lucide/arrow-left';
+
+  let isFirstVisit = $state(false);
+
+  onMount(() => {
+    if (history.length == 1) {
+      isFirstVisit = true;
+    }
+  });
+</script>
+
+{#if isFirstVisit}
+  <a
+    href="/blog/film/page/1"
+    class="flex items-center text-gray-700 hover:text-black"
+  >
+    <ArrowLeftIcon class="mr-2" />
+    Back to Gallery
+  </a>
+{:else}
+  <button
+    onclick={() => history.back()}
+    class="flex items-center text-gray-700 hover:text-black"
+  >
+    <ArrowLeftIcon class="mr-2" />
+    Back to Gallery
+  </button>
+{/if}
