1229b8bd7da4a1310955174a7019993eef59f3ea
Tyson <TysonS.417@gmail.com>
2025-01-26 01:08:00 -0700

    set up full height layout

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtPage />
+    <Head>
+      <link rel="icon" href="/img/icons/avi.svg" />
+    </Head>
+    <Body class="min-h-full flex flex-col scroll-smooth">
+      <main class="flex-grow selection:bg-primary selection:text-white">
+        <NuxtLoadingIndicator />
+        <NuxtLayout>
+          <NuxtPage />
+        </NuxtLayout>
+        <UNotifications />
+      </main>
+    </Body>
   </div>
 </template>
