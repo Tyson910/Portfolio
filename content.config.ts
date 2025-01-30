66191d0fc8f0513bc8849276cc331c7eb622a467
Tyson <TysonS.417@gmail.com>
2025-01-29 20:51:22 -0700

    add film content config option

--- content.config.ts
+++ content.config.ts
@@ -17,7 +17,7 @@ export default defineContentConfig({
       }),
     }),
     projects: defineCollection({
-      source: 'projects/**/*.yml',
+      source: "projects/**/*.yml",
       type: "data",
       schema: z.object({
         name: z.string(),
@@ -26,6 +26,34 @@ export default defineContentConfig({
         techUsed: z.array(z.string()),
         deployURL: z.string().url(),
         sourceCodeURL: z.string().url().nullable(),
+      }),
+    }),
+    photos: defineCollection({
+      source: "film/**/*.json",
+      type: "data",
+      schema: z.object({
+        date: z.string().datetime(),
+        src: z.string().startsWith('/'),
+        title: z.string(),
+        location: z.string(),
+        description: z.string().optional(),
+        tags: z
+          .enum([
+            "Landscape",
+            "Architecture",
+            "Art",
+            "Graffiti",
+            "Nature / Wildlife",
+            "Misc",
+          ])
+          .array(),
+        filmType: z.enum([
+          "Kodak Gold 200",
+          "Kodak UltraMax 400",
+          "Kodak Gold 800 (Disposable)",
+          "Fuji Film 400",
+          "Kodak Professional T-Max 400 Black and White",
+        ]),
       }),
     }),
   },
