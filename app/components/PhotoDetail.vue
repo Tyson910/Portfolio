86691b31e5d44e1b87e8072ddb4241632174e1b1
Tyson <TysonS.417@gmail.com>
2025-01-29 22:49:10 -0700

    create PhotoDetail component

---
+++ app/components/PhotoDetail.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  icon: string;
+}>();
+</script>
+
+<template>
+  <div class="flex items-center text-sm text-gray-600 dark:text-gray-100">
+    <Icon :name="props.icon" class="mr-2 size-4 text-gray-500" />
+    <slot />
+  </div>
+</template>
