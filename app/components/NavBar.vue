5da8890b3696968370884d275387a7e32786bea3
Tyson <TysonS.417@gmail.com>
2025-01-29 18:05:45 -0700

    add NavBar

---
+++ app/components/NavBar.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+interface NavigationLink {
+  href: string;
+  /** What is shown to end user */
+  text: string;
+}
+
+const navigationLinks = [
+  {
+    href: "/",
+    text: "Home",
+  },
+  {
+    href: "/about",
+    text: "About",
+  },
+  // {
+  //   href: '/projects',
+  //   text: 'Projects',
+  // },
+  {
+    href: "/blog",
+    text: "Blog",
+  },
+  // {
+  //   href: '/contact',
+  //   text: 'Contact',
+  // },
+] as const satisfies NavigationLink[];
+</script>
+
+<template>
+  <header class="py-4 w-full sticky top-0 z-10">
+    <nav
+      class="w-max mx-auto gap-x-6 px-8 flex justify-evenly bg-primary shadow-md lg:shadow-sm rounded-full"
+    >
+      <a
+        v-for="{ href, text } in navigationLinks"
+        :key="href"
+        :href="href"
+        class="hover:underline py-4 text-sm font-medium text-white hover:text-purple-100"
+      >
+        {{ text }}
+      </a>
+    </nav>
+  </header>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,14 @@
+<script setup lang="ts">
+import NavBar from "~/components/NavBar.vue";
+import Footer from "~/components/Footer.vue";
+</script>
+
 <template>
-  <div class="py-10">
-    <slot />
+  <div>
+    <NavBar />
+    <div class="py-10">
+      <slot />
+    </div>
+    <Footer />
   </div>
-  <Footer />
 </template>
