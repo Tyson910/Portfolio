976b6e2277472fc62035ab53bd88dc478befc58f
Tyson <TysonS.417@gmail.com>
2025-02-08 21:57:15 -0700

    fix file path typo

--- migrate.ts
+++ migrate.ts
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
-import data from "./src/content/photos.json";
+import data from "./photos.legacy.json";
 
 
 function capitalizeFirstLetter(str: string) {
