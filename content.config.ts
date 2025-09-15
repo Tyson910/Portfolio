5d8560f6533704ba5371e874a1e93d29bcd28c41
Tyson <TysonS.417@gmail.com>
2025-09-15 13:21:11 -0700

    remove film code & images

--- content.config.ts
+++ content.config.ts
@@ -31,37 +31,5 @@ export default defineContentConfig({
         sourceCodeURL: z.string().url().nullable(),
       }),
     }),
-    photos: defineCollection(
-      asSitemapCollection({
-        source: "film/**/*.json",
-        type: "page",
-        schema: z.object({
-          date: z.string().date(),
-          src: z.string().startsWith("/"),
-          title: z.string(),
-          location: z.string(),
-          description: z.string().optional(),
-          isPotraitOrientation: z.boolean().default(false),
-          tags: z
-            .enum([
-              "Landscape",
-              "Architecture",
-              "Art",
-              "Graffiti",
-              "Nature / Wildlife",
-              "Food",
-              "Misc",
-            ])
-            .array(),
-          filmType: z.enum([
-            "Kodak Gold 200",
-            "Kodak UltraMax 400",
-            "Kodak Gold 800 (Disposable)",
-            "Fuji Film 400",
-            "Kodak Professional T-Max 400 Black and White",
-          ]),
-        }),
-      })
-    ),
   },
 });
