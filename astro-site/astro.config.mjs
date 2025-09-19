8f0c01bb0992a67f805d5f374c1103c4e91223c0
Tyson <TysonS.417@gmail.com>
2025-09-19 09:00:46 -0700

    add tw and vue

--- astro-site/astro.config.mjs
+++ astro-site/astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 
 import mdx from "@astrojs/mdx";
+import vue from "@astrojs/vue";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +13,8 @@ export default defineConfig({
     port: 3000,
   },
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), vue()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
