235838455ab146bc4c56fb71c0eb357b6a51e1ab
Tyson <TysonS.417@gmail.com>
2025-09-19 09:14:39 -0700

    set up expressive code

--- astro-site/astro.config.mjs
+++ astro-site/astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import vue from "@astrojs/vue";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
+import expressiveCode from "astro-expressive-code";
 
 import tailwindcss from "@tailwindcss/vite";
 
@@ -13,7 +14,21 @@ export default defineConfig({
     port: 3000,
   },
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), vue()],
+  integrations: [
+    expressiveCode({
+      defaultProps: {
+        // Enable word wrap by default
+        wrap: true,
+        // Disable wrapped line indentation for terminal languages
+        overridesByLang: {
+          "bash,ps,sh": { preserveIndent: false },
+        },
+      },
+    }),
+    mdx(),
+    sitemap(),
+    vue(),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
