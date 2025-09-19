c32a4ad7608ca5e17b6892337455bea6bed75b39
Tyson <TysonS.417@gmail.com>
2025-09-18 21:18:09 -0700

    migrate content config

--- content.config.ts
+++ content.config.ts
@@ -1,8 +1,25 @@
-import { defineContentConfig, defineCollection, z } from "@nuxt/content";
+import { defineContentConfig, defineCollection } from "@nuxt/content";
 import { asSitemapCollection } from "@nuxtjs/sitemap/content";
+import { z } from 'zod';
 
 export default defineContentConfig({
   collections: {
+    blogs: defineCollection(
+      asSitemapCollection({
+        source: {
+          include: "blog/**/*.md",
+          prefix: "/blog",
+        },
+        type: "page",
+        schema: z.object({
+          dateCreated: z.string().date(),
+          lastUpdated: z.string().date(),
+          tags: z.string().nonempty().array(),
+          isDraft: z.boolean().default(true),
+          isFeaturedPost: z.boolean().default(false),
+        }),
+      })
+    ),
     snippets: defineCollection(
       asSitemapCollection({
         source: {
